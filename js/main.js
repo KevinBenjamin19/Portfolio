a4e61560a235ea014ffa689a95bbdd1d1b524f1e
kevin <kevinbusiness72@gmail.com>
2023-03-02 16:14:36 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+const toggle = document.querySelector(".navbar")
+
+const toggleMenuOpen = () => document.body.classList.toggle("open");
+
+
+
+
+
 var typed = new Typed(".multiple-text", {
     strings: ["Frontend Developer", "Web Designer"],
     typeSpeed: 100,
@@ -6,3 +14,25 @@ var typed = new Typed(".multiple-text", {
     loop: true
 })
 
+const sendMail = () => {
+    var params = {
+        name: document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value,
+    }
+    
+    const serviceID = "service_8kpxewr";
+    const templateID ="template_7vyqgon";
+    
+    emailjs.send(serviceID, templateID, params)
+    .then(
+        res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message sent Successfully");
+        }
+        )
+        .catch( err => console.log(err) );
+}
